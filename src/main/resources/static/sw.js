9c836285906429d22f73782c21d19c4396c43239
BNouwt <BNouwt@192.168.2.17>
2025-05-21 15:48:13 +0200

    Now PWA is working pretty well.
    Does not work yet when exposed via router, but does work using port
    forwarding feature of Chrome to mobile android device.

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,51 +1,63 @@
 self.addEventListener("install", function(event) {
-  event.waitUntil(preLoad());
+	event.waitUntil(preLoad());
 });
 
-var preLoad = function(){
-  console.log("Installing web app");
-  return caches.open("offline").then(function(cache) {
-    console.log("caching index and important routes");
-    return cache.addAll(["/index.html", "/offline.html"]);
-  });
+const cachedFiles = [
+	"/index.html",
+	"/offline.html",
+	"vuetify.min.css",
+	"materialdesignicons.min.css",
+	"vue.esm-browser.js",
+	"/vuetify.esm.js",
+	"/fonts/materialdesignicons-webfont.woff2?v=5.9.55",
+	"/manifest.json",
+	"/event.png"
+];
+
+var preLoad = function() {
+	console.log("Installing web app");
+	return caches.open("offline").then(function(cache) {
+		console.log("caching index and important routes");
+		return cache.addAll(cachedFiles);
+	});
 };
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request);
-  }));
-  event.waitUntil(addToCache(event.request));
+	event.respondWith(checkResponse(event.request).catch(function() {
+		return returnFromCache(event.request);
+	}));
+	event.waitUntil(addToCache(event.request));
 });
 
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
-  });
+var checkResponse = function(request) {
+	return new Promise(function(fulfill, reject) {
+		fetch(request).then(function(response) {
+			if (response.status !== 404) {
+				fulfill(response);
+			} else {
+				reject();
+			}
+		}, reject);
+	});
 };
 
-var addToCache = function(request){
-  return caches.open("offline").then(function (cache) {
-    return fetch(request).then(function (response) {
-      console.log(response.url + " was cached");
-      return cache.put(request, response);
-    });
-  });
+var addToCache = function(request) {
+	return caches.open("offline").then(function(cache) {
+		return fetch(request).then(function(response) {
+			console.log(response.url + " was cached");
+			return cache.put(request, response);
+		});
+	});
 };
 
-var returnFromCache = function(request){
-  return caches.open("offline").then(function (cache) {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
-     } else {
-       return matching;
-     }
-    });
-  });
+var returnFromCache = function(request) {
+	return caches.open("offline").then(function(cache) {
+		return cache.match(request).then(function(matching) {
+			if (!matching || matching.status == 404) {
+				return cache.match("offline.html");
+			} else {
+				return matching;
+			}
+		});
+	});
 };
